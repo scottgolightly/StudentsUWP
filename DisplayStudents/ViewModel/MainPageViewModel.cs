762eaf732b3260d5423bcb1d02643d66f41e5b8e
Scott Golightly <scott_golightly@hotmail.com>
2017-03-01 19:50:40 -0700

    Minor code cleanup

--- DisplayStudents/ViewModel/MainPageViewModel.cs
+++ DisplayStudents/ViewModel/MainPageViewModel.cs
@@ -1,11 +1,7 @@
 ﻿using DisplayStudents.Model;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DisplayStudents.ViewModel
 {
